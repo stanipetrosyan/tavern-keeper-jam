e997b2f314b6752523a9e686efd154ca9926709c
stanipetrosyan <stani.petrosyan@gmail.com>
2024-12-03 22:13:47 +0100

    ui: started ingredient generation

--- Assets/Scripts/ShopMenuPopup.cs
+++ Assets/Scripts/ShopMenuPopup.cs
@@ -1,14 +1,29 @@
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+using Image = UnityEngine.UI.Image;
 
 public class ShopMenuPopup : MonoBehaviour {
     [SerializeField] private Ingredient[] ingredients;
     
 
     public void Start() {
+        float x = 1;
+        float y = 2;
         
+        foreach (var item in ingredients) {
+            GameObject newObj = new GameObject(item.name);
+            Image image = newObj.AddComponent<Image>();
+            image.sprite = item.image;
+            newObj.GetComponent<RectTransform>().SetParent(transform); //Assign the newly created Image GameObject as a Child of the Parent Panel.
+            newObj.GetComponent<RectTransform>().sizeDelta = new Vector2(64, 64);
+            newObj.GetComponent<RectTransform>().localScale = new Vector3(0.1f, 0.1f, 0.1f);
+            newObj.GetComponent<RectTransform>().position = new Vector2(x, y);
+            newObj.SetActive(true); //Activate the GameObject
+
+            x += 2;
+        }
     }
-    
+
     public void Open() {
         gameObject.SetActive(true);
     }
