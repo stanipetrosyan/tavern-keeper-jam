3d04e15c24f030ac3c36bf827b54578256e63ca7
stanipetrosyan <stani.petrosyan@gmail.com>
2024-12-10 22:30:46 +0100

    feat: fixed inventory generation in kitchen

--- Assets/Scripts/UI/KitchenPopup.cs
+++ Assets/Scripts/UI/KitchenPopup.cs
@@ -1,12 +1,17 @@
-using System;
+using System.Collections.Generic;
 using Cards;
 using Manager;
 using ScriptableObjects;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace UI{
     public class KitchenPopup : UiPopup{
+        private Ingredient[] _ingredients;
+        [SerializeField] GameObject inventoryCardPrefab;
+        [SerializeField] GameObject inventory;
+        private GameObject _ingredientCard;
+        private List<GameObject> _objects;
+        
         public override void Open() {
             gameObject.SetActive(true);
         }
@@ -14,41 +19,42 @@ namespace UI{
         public override void Close() {
             gameObject.SetActive(false);
         }
-
-        private Ingredient[] ingredients;
-        [SerializeField] GameObject inventoryCardPrefab;
-        [SerializeField] GameObject inventory;
-        private GameObject ingredientCard;
         
-        private void GenerateInventory() {
-            ingredients = Managers.Inventory.GetIngredients();
-            float x = -200;
-            float y = -200;
-
-            foreach (var item in ingredients) {
-                ingredientCard = Instantiate(inventoryCardPrefab);
-
-                ingredientCard.GetComponent<InventoryCard>().SetIngredient(item);
-
-
-                ingredientCard.transform.SetParent(inventory.transform, false);
-                ingredientCard.transform.localScale = new Vector3(1, 1, 1);
-                ingredientCard.transform.localPosition = new Vector3(x, y, 0);
-
-
-                x += 200;
-            }
-        }
-
         private void OnEnable() {
+            _objects = new List<GameObject>();
             GenerateInventory();
         }
 
         private void OnDisable() {
-            // Clean all objects created
-            throw new NotImplementedException();
+            foreach (var item in _objects) {
+                Destroy(item);
+            }
+            
+            _objects.Clear();
         }
 
+        
+        private void GenerateInventory() {
+            _ingredients = Managers.Inventory.GetIngredients();
+            float x = -200;
+            float y = -200;
+
+            foreach (var item in _ingredients) {
+                _ingredientCard = Instantiate(inventoryCardPrefab);
+                
+                _objects.Add(_ingredientCard.gameObject);
+                
+                _ingredientCard.GetComponent<InventoryCard>().SetIngredient(item);
+
+
+                _ingredientCard.transform.SetParent(inventory.transform, false);
+                _ingredientCard.transform.localScale = new Vector3(1, 1, 1);
+                _ingredientCard.transform.localPosition = new Vector3(x, y, 0);
+
+                
+                x += 200;
+            }
+        }
         public void AddRecipeToInventory() {
             Managers.Inventory.AddRecipe(Managers.Kitchen.ActualRecipe());
         }
