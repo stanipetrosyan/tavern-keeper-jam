4e5668eb49059803923c8793b28eee0efe208bd2
Iustin Costin Neaga <iustin.neaga@cherrychain.it>
2024-12-27 20:37:01 +0100

    added cooker and beerer

--- Assets/Scripts/Jack/PlayerController.cs
+++ Assets/Scripts/Jack/PlayerController.cs
@@ -1,19 +1,34 @@
 using John;
 using Manager;
+using UI;
 using UnityEngine;
 
 namespace Jack{
     public class PlayerController : MonoBehaviour {
 
-        [SerializeField] private float moveSpeed = 3f;
+        [SerializeField] private float moveSpeed = 5f;
         [SerializeField] private Transform movePoint;
-        [SerializeField] private LayerMask decorationLayer;
+        [SerializeField] private LayerMask furnitureLayer;
         [SerializeField] private LayerMask interactLayer;
+        [SerializeField] private LayerMask cookLayer;
+        [SerializeField] private LayerMask beerLayer;
+        [SerializeField] private KitchenPopup kitchenPopup;
+        [SerializeField] private KitchenPopup beerPopup;
         [SerializeField] private float radius; 
+        
+        private Vector2 facingDirection;
 
     
         void Start() {
             movePoint.parent = null;
+        }
+
+        void Stop() {
+            moveSpeed = 0f;
+        }
+
+        void Move() {
+            moveSpeed = 5f;
         }
 
         void Update() {
@@ -23,27 +38,45 @@ namespace Jack{
             if (!(Vector3.Distance(transform.position, movePoint.position) < 0.5f)) return;
             
             if (Mathf.Approximately(Mathf.Abs(Input.GetAxisRaw("Horizontal")), 1f)) {
-                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), radius, decorationLayer)) {
+                facingDirection = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+                Debug.Log($"Facing direction is now ${facingDirection}");
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), radius, furnitureLayer)) {
                     movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
                 }
             }
 
             if (Mathf.Approximately(Mathf.Abs(Input.GetAxisRaw("Vertical")), 1f)) {
-                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), radius, decorationLayer)) {
+                facingDirection = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+                Debug.Log($"Facing direction is now ${facingDirection}");
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), radius, furnitureLayer)) {
                     movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
                 }
             }
 
-            var hit = Physics2D.CircleCast(movePoint.position, 0.5f, Vector2.up, 0.5f, interactLayer);
-            if (hit.collider != null) {
+            var hitCustomer = Physics2D.CircleCast(movePoint.position, 0.5f, facingDirection, 0.5f, interactLayer);
+            if (hitCustomer.collider != null) {
                 if (Input.GetKeyDown(KeyCode.E)) {
-                    var customer = hit.collider.gameObject.GetComponent<CustomerController>();
+                    var customer = hitCustomer.collider.gameObject.GetComponent<CustomerController>();
                     var order = customer.GetOrder();
                     var recipe = Managers.Inventory.GetRecipe(order.name);
                     if (customer.Interact(recipe)) {
                         Managers.Tavern.AddMoney(recipe.cost);
                         Managers.Inventory.RemoveRecipe(recipe);
                     }
+                }
+            }
+            var hitCook = Physics2D.CircleCast(movePoint.position, 0.5f, facingDirection, 0.5f, cookLayer);
+            if (hitCook.collider != null) {
+                if (Input.GetKeyDown(KeyCode.E)) {
+                    Debug.Log("Open Cooker");
+                    kitchenPopup.Open();
+                }
+            }
+            var hitBeer = Physics2D.CircleCast(movePoint.position, 0.5f, facingDirection, 0.5f, beerLayer);
+            if (hitBeer.collider != null) {
+                if (Input.GetKeyDown(KeyCode.E)) { 
+                    Debug.Log("Open Beerer"); 
+                    beerPopup.Open();
                 }
             }
         }
